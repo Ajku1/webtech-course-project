3c1b7f1a6ee55e4c4828c88b0f42625b7c5b669f
G-Toncheva <67205740+G-Toncheva@users.noreply.github.com>
2021-05-29 15:54:46 +0300

    Feature/fn71901 chats page (#13)
    * Add files via upload
    
    * Update messages.css
    
    * Update messages.css
    
    * Add files via upload
    
    * Delete src/app/Message directory
    
    * Update chats-page.component.html
    
    * chat page button logout
    
    * chats page
    
    * resolve comments
    
    * frontend
    
    * buttons connection
    
    * chats page navigation
    
    * navigation page
    
    Co-authored-by: Nikola Kostadinov <nikola_sk@abv.bg>

--- frontend/src/app/route.enum.ts
+++ frontend/src/app/route.enum.ts
@@ -2,5 +2,7 @@ export enum Route {
     Home = 'home',
     Login = 'login',
     Register = 'register',
-    CreateChatroomModal = 'create-chatroom-modal'
+    CreateChatroomModal = 'create-chatroom-modal',
+    ChatsPage = 'app-chats-page',
+    Chatroom = 'app-chatroom',
 }
