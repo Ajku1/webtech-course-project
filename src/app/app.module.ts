c1fc3276ab0123bad0a4bdf87439e9d8d4581c54
Hristiyan-Petkov <haripetkov991226@gmail.com>
2021-05-29 11:33:01 +0300

    Feature/fn71865 chatroom design (#6)
    * Inital commit, chat room design.
    
    * Bug fix.
    
    * fix conflicts
    
    Co-authored-by: Nikola Kostadinov <nikola_sk@abv.bg>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { ChatsPageComponent } from './chats-page/chats-page.component';
-import { CreateChatroomModalComponent } from './create-chatroom-modal/create-chatroom-modal.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {ChatsPageComponent} from './chats-page/chats-page.component';
+import {CreateChatroomModalComponent} from './create-chatroom-modal/create-chatroom-modal.component';
 
 @NgModule({
     declarations: [
@@ -24,10 +24,11 @@ import { CreateChatroomModalComponent } from './create-chatroom-modal/create-cha
         AppRoutingModule,
         ReactiveFormsModule,
         FormsModule,
-        Ng2SearchPipeModule 
+        Ng2SearchPipeModule
     ],
     providers: [],
     bootstrap: [AppComponent]
-  })
+})
 
-export class AppModule { }
+export class AppModule {
+}
