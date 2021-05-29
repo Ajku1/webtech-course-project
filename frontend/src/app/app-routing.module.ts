f95aa674f97d9643dcf61b9159355231b5ee47d7
G-Toncheva <67205740+G-Toncheva@users.noreply.github.com>
2021-05-29 15:54:46 +0300

    Feature/fn71901 chats page (#13)
    * Add files via upload
    
    * Update messages.css
    
    * Update messages.css
    
    * Add files via upload
    
    * Delete src/app/Message directory
    
    * Update chats-page.component.html
    
    * chat page button logout
    
    * chats page
    
    * resolve comments
    
    * frontend
    
    * buttons connection
    
    * chats page navigation
    
    * navigation page
    
    Co-authored-by: Nikola Kostadinov <nikola_sk@abv.bg>

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { RegisterComponent } from './register/register.component';
 import { CreateChatroomModalComponent } from './create-chatroom-modal/create-chatroom-modal.component';
 import { Route } from "./route.enum";
 import { ChatsPageComponent } from './chats-page/chats-page.component';
+import { ChatroomComponent } from './chatroom/chatroom.component';
 
 const routes: Routes = [
     {
@@ -20,18 +21,22 @@ const routes: Routes = [
         path: Route.Register,
         component: RegisterComponent
     },
-    {
-        path: '**',
-        component: HomeComponent
-    },
     { 
-        path: 'app-chats-page', 
+        path: Route.ChatsPage, 
         component: ChatsPageComponent
     },
     {
       path: Route.CreateChatroomModal,
       component: CreateChatroomModalComponent
-    }
+    },
+    {
+      path: Route.Chatroom,
+      component: ChatroomComponent
+    },
+    {
+        path: '**',
+        component: HomeComponent
+    },
 ];
 
 @NgModule({
