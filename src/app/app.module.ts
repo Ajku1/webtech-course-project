e58a200bf5b18c8579c51df402eb1232c2e3aa70
Nikola Kostadinov <nikola_sk@abv.bg>
2021-05-29 11:32:17 +0300

    fix conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'; 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { Ng2SearchPipeModule } from 'ng2-search-filter';
-import { HomeComponent } from './home/home.component';
-import { RegisterComponent } from './register/register.component';
-import { LoginComponent } from './login/login.component';
-import { ChatsPageComponent } from './chats-page/chats-page.component';
-import { CreateChatroomModalComponent } from './create-chatroom-modal/create-chatroom-modal.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {Ng2SearchPipeModule} from 'ng2-search-filter';
+import {HomeComponent} from './home/home.component';
+import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './login/login.component';
+import {ChatsPageComponent} from './chats-page/chats-page.component';
+import {CreateChatroomModalComponent} from './create-chatroom-modal/create-chatroom-modal.component';
 
 @NgModule({
     declarations: [
@@ -25,28 +24,11 @@ import { CreateChatroomModalComponent } from './create-chatroom-modal/create-cha
         AppRoutingModule,
         ReactiveFormsModule,
         FormsModule,
-        Ng2SearchPipeModule 
+        Ng2SearchPipeModule
     ],
     providers: [],
     bootstrap: [AppComponent]
-  })
-
-export class AppModule { }
-=======
-import { ChatroomComponent } from './chatroom/chatroom.component';
-
-@NgModule({
-  declarations: [
-    AppComponent,
-    ChatroomComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
 })
+
 export class AppModule {
 }
->>>>>>> fdd2960... Bug fix.
