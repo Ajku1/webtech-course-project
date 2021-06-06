e8f49733cbe9acef57764880ba7e7479e9bbb605
Denitsa Stoyanova <denitsa.stoianova@abv.bg>
2021-06-06 22:25:27 +0300

    remove user id

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -3,10 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 
 const userSchema = new mongoose.Schema(
   {
-    _id: {
-      type: String,
-      default: () => uuidv4().replace(/\-/g, ""),
-    },
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
