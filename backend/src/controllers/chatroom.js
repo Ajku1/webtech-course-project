e17a3918d7ac4970ee90a48ff1027aa3bb9a55d3
G-Toncheva <67205740+G-Toncheva@users.noreply.github.com>
2021-06-14 23:46:55 +0300

    Feature/fn71901 chats page (#28)
    * Add files via upload
    
    * Update messages.css
    
    * Update messages.css
    
    * Add files via upload
    
    * Delete src/app/Message directory
    
    * Update chats-page.component.html
    
    * chat page button logout
    
    * chats page
    
    * resolve comments
    
    * frontend
    
    * buttons connection
    
    * chats page navigation
    
    * navigation page
    
    * chats page
    
    * remove code comment
    
    * search
    
    * problem
    
    * fix build
    
    * chat id
    
    * login message
    
    * login message
    
    Co-authored-by: Nikola Kostadinov <nikola_sk@abv.bg>

--- backend/src/controllers/chatroom.js
+++ backend/src/controllers/chatroom.js
@@ -1,7 +1,7 @@
 import Chatroom from '../models/chatroom.js';
 
 export default {
-  createChatroom: (req, res) => {  
+  createChatroom: (req, res) => {
     const chatroom = new Chatroom({
         name: req.body.name,
         members: req.body.members,
@@ -26,7 +26,7 @@ export default {
         if (error) {
             res.status(500).json({ result: false, message: 'Unable to get created chatrooms', error });
         }
-        res.status(200).json({ result: true, chatrooms: createdChatrooms });
+        res.status(200).json({ chatrooms: createdChatrooms });
     });
   }
 };
--- frontend/src/app/chats-page/chats-page.component.ts
+++ frontend/src/app/chats-page/chats-page.component.ts
@@ -1,21 +1,82 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 import { Route } from '../route.enum';
+import { Chatroom } from './chatroom.interface';
 
 @Component({
     selector: 'app-chats-page',
     templateUrl: './chats-page.component.html',
     styleUrls: ['./chats-page.component.css']
 })
-export class ChatsPageComponent {
-    constructor(private readonly router: Router) {
+export class ChatsPageComponent implements OnInit {
+    chatrooms!: Chatroom[];
+    chatroomId!: String;
+
+    constructor(private router: Router,
+                private readonly httpClient: HttpClient) {
+        console.log(this.router.url);
+        this.getAllChatsWithMember();
     }
 
-    onChat() {
+    getAllChatsWithMember() {
+        const chatroomsObservable = this.httpClient.get('/api/chatroom') as Observable<{ chatrooms: Chatroom[] }>;
+        chatroomsObservable.subscribe((res: { chatrooms: Chatroom[] }) => {
+            this.chatrooms = res.chatrooms;
+            console.log(this.chatrooms);
+        });
+    }
+
+    getElementByCriteria() {
+        const input = <HTMLInputElement>document.getElementById('search');
+
+        for (let i = 0; i < this.chatrooms.length; i++) {
+            const button = document.getElementById(`chat_${i}`);
+
+            if (button != null) {
+                if (input.value == '') {
+                    button.style.visibility = 'visible';
+                } else if (button?.textContent == input.value) {
+                    button.style.visibility = 'visible';
+                } else if (button?.textContent != input.value) {
+                    if (button?.textContent?.substring(0, input.value.length) == input.value) {
+                        button.style.visibility = 'visible';
+                    } else {
+                        button.style.visibility = 'hidden';
+                    }
+                }
+            }
+        }
+    }
+
+    arrayElementOnIndex(n: number): string {
+        return this.chatrooms[n].name;
+    }
+
+    arrayChatCount(): Array<number> {
+        return Array(this.chatrooms.length);
+    }
+
+    onLogout() {
+        this.router.navigate([Route.Home]);
+    }
+
+    onChat(id: string) {
+        for (let index = 0; index < this.chatrooms.length; index++) {
+            if(this.chatrooms[index].name == document.getElementById(id)?.innerText)
+            {
+                this.chatroomId = this.chatrooms[index]._id;
+            }
+        }
+        
         this.router.navigate([Route.Chatroom]);
     }
 
     onCreate() {
         this.router.navigate([Route.CreateChatroomModal]);
+    }
+
+    ngOnInit() {
     }
 }
